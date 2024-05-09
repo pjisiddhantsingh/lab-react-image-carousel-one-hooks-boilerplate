27de7ee83327c0d9f8b35a4637633adbc2fbd21c
pjisiddhantsingh <159141612+pjisiddhantsingh@users.noreply.github.com>
2024-05-09 10:37:03 +0530

    Update Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
-
-// you can research - how to use material ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// complete the function below:
 function Carousel() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const goToPrevImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  const goToNextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  return (
+    <div className="carousel">
+      <button className="prev-btn" onClick={goToPrevImage}><ArrowBackIosIcon /></button>
+      <img src={images[currentImageIndex]} alt={`Image ${currentImageIndex}`} className="image" />
+      <button className="next-btn" onClick={goToNextImage}><ArrowForwardIosIcon /></button>
+    </div>
+  );
 }
 
 export default Carousel;
